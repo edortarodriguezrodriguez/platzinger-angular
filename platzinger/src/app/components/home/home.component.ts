2e08469d0eb0924755ab950aee2185dcff3a9fa1
rodriguez <e.rodriguez@ariadnagrid.com>
2019-11-21 14:36:15 +0100

    ngfor

--- platzinger/src/app/components/home/home.component.ts
+++ platzinger/src/app/components/home/home.component.ts
@@ -8,15 +8,54 @@ import {User} from "../../interfaces/user";
 })
 export class HomeComponent implements OnInit {
 
+  friends: User [];
+
   constructor() {
     let myUser: User = {
-      nick: 'burdaich',
+      nick: 'paco',
       subnick: 'hola',
       age: 30,
       email: 'aa@aa.com',
       friend: true,
       uid: 1
     };
+
+    let myUser1: User = {
+      nick: 'maria',
+      subnick: 'hola',
+      age: 30,
+      email: 'aa@aa.com',
+      friend: true,
+      uid: 1
+    };
+    let myUser2: User = {
+      nick: 'nicki',
+      subnick: 'hola',
+      age: 30,
+      email: 'aa@aa.com',
+      friend: true,
+      uid: 1
+    };
+    let myUser3: User = {
+      nick: 'patxi',
+      subnick: 'hola',
+      age: 30,
+      email: 'aa@aa.com',
+      friend: true,
+      uid: 1
+    };
+    let myUser4: User = {
+      nick: 'pacotabaco',
+      subnick: 'hola',
+      age: 30,
+      email: 'aa@aa.com',
+      friend: true,
+      uid: 1
+    };
+
+
+    this.friends = [myUser, myUser1, myUser2, myUser3, myUser4]
+
 
     console.log(myUser);
 
