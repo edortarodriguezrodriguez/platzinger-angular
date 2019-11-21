2eaeb483e47a5553f1580bd0586a3d96ca7fb59d
rodriguez <e.rodriguez@ariadnagrid.com>
2019-11-21 10:00:30 +0100

    Creating routing between components

--- platzinger/src/app/app.module.ts
+++ platzinger/src/app/app.module.ts
@@ -7,6 +7,16 @@ import {LoginComponent} from './components/login/login.component';
 import {HomeComponent} from './components/home/home.component';
 import {ConversationComponent} from './components/conversation/conversation.component';
 import {ProfileComponent} from './components/profile/profile.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -18,7 +28,8 @@ import {ProfileComponent} from './components/profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
