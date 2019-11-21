2929cbcc65c408df9dcc031d6c7a7c9fe757e1d8
rodriguez <e.rodriguez@ariadnagrid.com>
2019-11-21 10:55:08 +0100

    how to use interface in typescript

--- platzinger/src/app/components/home/home.component.ts
+++ platzinger/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from "../../interfaces/user";
 
 @Component({
   selector: 'app-home',
@@ -8,32 +9,20 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() {
-    let c: number = 1;
-    let b: number = 2;
+    let myUser: User = {
+      nick: 'burdaich',
+      subnick: 'hola',
+      age: 30,
+      email: 'aa@aa.com',
+      friend: true,
+      uid: 1
+    };
 
-    let e: string = '1';
-    let f: string = '2';
-    console.log(e + f);
+    console.log(myUser);
 
-    let g: boolean = true;
-    let h: object = {};
+    let users: User  []
+      = [myUser];
 
-    console.log(g);
-    console.log(h);
-
-    let i = [c, b, e, f, g, h];
-
-    console.log(i);
-
-    let j: boolean [] = [true, g];
-
-    console.log(j);
-
-    let k: object [] = [h, j];
-
-    let l: any = [c, b, e, f, g, h, j, k];
-
-    console.log(l);
   }
 
   ngOnInit() {
