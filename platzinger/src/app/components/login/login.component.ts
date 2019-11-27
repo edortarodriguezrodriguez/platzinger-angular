ea10240c45ac41b9483f3ea61a5ff0170aae8be7
rodriguez <e.rodriguez@ariadnagrid.com>
2019-11-27 12:18:48 +0100

    Add auth firebase

--- platzinger/src/app/components/login/login.component.ts
+++ platzinger/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   operation: string = 'login';
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authenticationService.loginWithEmail(this.email, this.password).then((data) => {
+      alert("logeado correctamente");
+      console.log(data);
+    }).catch((error) => {
+      alert('Ocurrio un error');
+      console.log(error)
+    });
+  }
+
+  register() {
+    this.authenticationService.registerWithEmail(this.email, this.password).then((data) => {
+      alert("registrado correctamente");
+      console.log(data);
+    }).catch((error) => {
+      alert('Ocurrio un error');
+      console.log(error)
+    });
   }
 
 }
